f86900c6680f69018ae83f588b5133c0cc6e7fd3
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2024-04-19 18:21:43 +0900

    create時にISGをクリア

--- app/api/name/route.ts
+++ app/api/name/route.ts
@@ -1,6 +1,7 @@
 import type { generatetype } from "@/app/(sinndann)/name/[id]/mytype";
 import { createClient } from "@supabase/supabase-js";
 import { type NextRequest, NextResponse } from "next/server";
+import { revalidatePath } from "next/cache";
 
 const supabase = createClient(process.env.SUPABASE_URL ?? "", process.env.SUPABASE_SERVICE_ROLE_KEY ?? "");
 export async function POST(rawreq: NextRequest) {
@@ -21,6 +22,7 @@ export async function POST(rawreq: NextRequest) {
 				id = data.data[0].id;
 			}
 		});
+	await revalidatePath(`/name/${id}`)
 	if (iserror) return NextResponse.json({ error: error }, { status: 400 });
     if(id===0)return NextResponse.json(undefined, { status: 500 });
 	return NextResponse.json({id:id}, { status: 200 });
