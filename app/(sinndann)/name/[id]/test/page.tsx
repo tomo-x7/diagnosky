516a6ae2c80b390c9009878ee31aeb9c77bf6a84
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2024-04-26 18:02:13 +0900

    isr test

---
+++ app/(sinndann)/name/[id]/test/page.tsx
@@ -0,0 +1,12 @@
+
+export const revalidate = 60
+
+export default async function Page({ params }: { params: { id: string } }) {
+	
+	return (
+		<>
+			<div>id:{params.id}</div>
+            <div>{new Date().toString()}</div>
+		</>
+	);
+}
