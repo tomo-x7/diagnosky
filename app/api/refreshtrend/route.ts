d5c6fae05237df76ee651eb45cf4529da81da6ea
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2024-04-27 14:22:46 +0900

    トレンドのISRテスト

--- app/api/refreshtrend/route.ts
+++ app/api/refreshtrend/route.ts
@@ -1,4 +1,5 @@
 import { Receiver } from "@upstash/qstash";
+import { revalidatePath } from "next/cache";
 import { type NextRequest, NextResponse } from "next/server";
 
 const receiver = new Receiver({
@@ -8,7 +9,10 @@ const receiver = new Receiver({
 
 // ... in your request handler
 export async function POST(req: NextRequest) {
-	const signature = req.headers.get("Upstash-Signature") ?? "";
+	const signature = req.headers.get("Upstash-Signature");
+	if(!signature){
+		return new NextResponse(undefined,{status:401})
+	}
 	const body = await req.text();
 
 	const isValid = await receiver.verify({
@@ -16,7 +20,9 @@ export async function POST(req: NextRequest) {
 		signature,
 		url: "https://diagnosky.vercel.app/api/refreshtrend",
 	});
-	console.log(isValid)
-	console.log(body)
-	return NextResponse.json({ issuccess:isValid });
+	if(!isValid){
+		return new NextResponse(undefined,{status:403})
+	}
+	revalidatePath('/trend')
+	return new NextResponse();
 }
--- app/trend/page.tsx
+++ app/trend/page.tsx
@@ -2,10 +2,12 @@ import { Redis } from "@upstash/redis";
 import { TrendComponent } from "./trendcomponent";
 const redis = Redis.fromEnv();
 
+export const dynamic = 'force-static'
 export default async function Page() {
 	const data: Array<string> = await redis.zrange("trend", 0, '+inf', { byScore: true });
 	return (
 		<>
+		<div>{new Date().toISOString()}</div>
 			{data.map((value,index) => {
 				return <TrendComponent key={index.toString()} id={value} />;
 			})}
