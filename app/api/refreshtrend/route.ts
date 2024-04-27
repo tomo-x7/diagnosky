0034e78eeeaa5639edad351ce19bf1552adfc1c8
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2024-04-27 16:19:29 +0900

    データの削除機能

--- app/api/refreshtrend/route.ts
+++ app/api/refreshtrend/route.ts
@@ -1,7 +1,9 @@
 import { Receiver } from "@upstash/qstash";
 import { revalidatePath } from "next/cache";
 import { type NextRequest, NextResponse } from "next/server";
+import { Redis, type ScoreMember } from "@upstash/redis";
 
+const redis = Redis.fromEnv();
 const receiver = new Receiver({
 	currentSigningKey: process.env.QSTASH_CURRENT_SIGNING_KEY ?? "",
 	nextSigningKey: process.env.QSTASH_NEXT_SIGNING_KEY ?? "",
@@ -10,8 +12,8 @@ const receiver = new Receiver({
 // ... in your request handler
 export async function POST(req: NextRequest) {
 	const signature = req.headers.get("Upstash-Signature");
-	if(!signature){
-		return new NextResponse(undefined,{status:401})
+	if (!signature) {
+		return new NextResponse(undefined, { status: 401 });
 	}
 	const body = await req.text();
 
@@ -20,11 +22,23 @@ export async function POST(req: NextRequest) {
 		signature,
 		url: "https://diagnosky.vercel.app/api/refreshtrend",
 	});
-	if(!isValid){
-		return new NextResponse(undefined,{status:403})
+	if (!isValid) {
+		return new NextResponse(undefined, { status: 403 });
 	}
-	revalidatePath('/trend')
-	await new Promise((r) => setTimeout(r, 500));
-	await fetch('https://diagnosky.vercel.app/trend')
+
+	revalidatePath("/trend");
+	await new Promise((r) => setTimeout(r, 100));
+	await fetch("https://diagnosky.vercel.app/trend");
+	await new Promise((r) => setTimeout(r, 100));
+	await fetch("https://diagnosky.vercel.app/trend");
+
+	const olddata: Array<string> = await redis.zrange("trend", 0, -1, { withScores: true });
+	const backupdata: Array<ScoreMember<string>> = [];
+	for (let i = 0; i < olddata.length / 2; i++) {
+		backupdata.push({ member: olddata[2 * i], score: Number.parseInt(olddata[2 * i + 1]) });
+	}
+	await redis.zadd("trendbackup", { member: "nodata", score: -5 }, ...backupdata);
+	await redis.del("trend");
+
 	return new NextResponse();
 }
