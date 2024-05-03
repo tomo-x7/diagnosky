73ea61ffdbff280d095495f49622413b1f0fb4bc
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2024-05-04 04:14:12 +0900

    Update page.tsx

--- app/(main)/create/name/page.tsx
+++ app/(main)/create/name/page.tsx
@@ -2,7 +2,7 @@ export const dynamic = 'force-static'
 import type { Metadata } from "next";
 import { Client } from "./client";
 const lists: Array<React.ReactNode> = [];
-export default function(){
+export default function Page(){
 	return <Client />
 }
 export const metadata:Metadata={
