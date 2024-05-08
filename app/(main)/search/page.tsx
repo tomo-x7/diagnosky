51807f7950fc6ce53a46d09ddcdaca8fb64b4364
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2024-05-08 23:09:08 +0900

    更新間隔の変更、再生成箇所の追加

--- app/(main)/search/page.tsx
+++ app/(main)/search/page.tsx
@@ -1,6 +1,6 @@
 import Trend from "../../trend/page";
 import { Tab } from "./tab";
-export const revalidate = 7200;
+export const revalidate = 3600;
 export const dynamic = "force-static";
 export default function Page() {
 	return (
--- app/api/name/route.ts
+++ app/api/name/route.ts
@@ -25,8 +25,9 @@ export async function POST(rawreq: NextRequest) {
 				id = data.data[0].id;
 			}
 		});
-	revalidatePath(`/name/${id}`);
 	await redis.lpush("latest", `name_${id}`);
+	revalidatePath(`/name/${id}`);
+	revalidatePath("/search/latest");
 	if (iserror) return NextResponse.json({ error: error }, { status: 400 });
 	if (id === 0) return NextResponse.json(undefined, { status: 500 });
 	return NextResponse.json({ id: id }, { status: 200 });
