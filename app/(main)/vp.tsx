03e55a30d1686fc81e99c41817aafe41c0c000b1
tomo_x <158121497+tomo-x7@users.noreply.github.com>
2024-05-24 13:08:18 +0900

    test

--- app/(main)/vp.tsx
+++ app/(main)/vp.tsx
@@ -1,35 +1,35 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export function VP({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
-	let vp:HTMLElement|null
+	let vp: HTMLElement | null
 	if (typeof document !== "undefined") {
 		vp = document.getElementById("vp");
 	}
-	let testscale="0"
+	const [testscale, settestscale] = useState('0')
 	const setwidth = () => {
 		if (typeof window !== "undefined") {
 			if (vp) {
-				const scale=(window.innerWidth > 500 ? 1 : Math.sqrt(window.innerWidth / 500)).toString()
+				const scale = (window.innerWidth > 500 ? 1 : Math.sqrt(window.innerWidth / 500)).toString()
 				vp.style.scale = scale;
-				testscale=scale
+				settestscale(scale)
 			}
 		}
 	};
 	if (typeof window !== "undefined") {
-	testscale=(window?.innerWidth??500 > 500 ? 1 : Math.sqrt(window?.innerWidth??100 / 500)).toString()
+		settestscale((window?.innerWidth ?? 500 > 500 ? 1 : Math.sqrt(window?.innerWidth ?? 100 / 500)).toString())
 	}
 	useEffect(() => {
 		setwidth();
 		window.addEventListener("resize", setwidth);
 	});
 	return (
-		<div id="vp" style={{ maxWidth: 500, width: "98vw",transformOrigin:"top center" }}>
+		<div id="vp" style={{ maxWidth: 500, width: "98vw", transformOrigin: "top center" }}>
 			{children}
 			<div>scale:{testscale}</div>
 		</div>
