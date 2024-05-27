db444dbe17f0f668fade25208b2e8089546f1c11
tomo_x <158121497+tomo-x7@users.noreply.github.com>
2024-05-27 15:35:48 +0900

    fix

--- app/(main)/login.tsx
+++ app/(main)/login.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-export function Login() {
+export function Logincomp() {
     return (<>
         <div id="background" className="fixed top-0 right-0 bottom-0 left-0 z-[100] bg-black bg-opacity-60" />
         <div>
