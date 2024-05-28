273d4051020027d00ec75f780e13c28d42fafc3e
tomo_x <158121497+tomo-x7@users.noreply.github.com>
2024-05-28 14:55:51 +0900

    テスト

--- app/(main)/login.tsx
+++ app/(main)/login.tsx
@@ -2,7 +2,7 @@
 
 export function Logincomp() {
     return (<>
-        <div id="background" className="fixed top-0 right-0 bottom-0 left-0 z-[100] bg-black opacity-50" />
+        <div id="background" className="fixed top-0 right-0 bottom-0 left-0 z-[100] bg-black opacity-50">background</div>
         <div className="relative bg-white z-[200]">
             <h2>Login with Bluesky account</h2>
             <input type="text" />
