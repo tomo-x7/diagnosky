8ae63f77ceb40ff4eb9e8e1ef6e02e5820d53233
tomo_x <158121497+tomo-x7@users.noreply.github.com>
2024-05-28 14:51:26 +0900

    スタイルの修正

--- app/(main)/login.tsx
+++ app/(main)/login.tsx
@@ -2,8 +2,8 @@
 
 export function Logincomp() {
     return (<>
-        <div id="background" className="fixed top-0 right-0 bottom-0 left-0 z-[100] bg-black bg-opacity-60" />
-        <div>
+        <div id="background" className="fixed top-0 right-0 bottom-0 left-0 z-[100] bg-black opacity-50" />
+        <div className="relative bg-white z-[200]">
             <h2>Login with Bluesky account</h2>
             <input type="text" />
             <input type="password" />
